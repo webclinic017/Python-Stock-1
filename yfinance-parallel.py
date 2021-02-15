57a42bee48c9828e640e967a1f9ebd3fbda2ee02
My digital passions <laferrierejc@gmail.com>
2021-02-15 06:24:22 -0600

    Update yfinance-parallel.py
    chose simpler method for pulling key stats

--- yfinance-parallel.py
+++ yfinance-parallel.py
@@ -1,3 +1,8 @@
+#inspiration
+#https://simply-python.com/2019/01/16/retrieving-stock-statistics-from-yahoo-finance-using-python/
+#https://towardsdatascience.com/backtest-your-trading-strategy-with-only-3-lines-of-python-3859b4a4ab44?source=friends_link&sk=ec647b6bb43fe322013248fd1d473015
+#https://simply-python.com/2019/01/16/retrieving-stock-statistics-from-yahoo-finance-using-python/
+
 !pip install --upgrade pip
 #!pip install 
 
@@ -140,209 +145,6 @@ for x in range(0,len(vetted_symbols)):
     stocks_data = pd.concat([stocks_data,df])
 
 stocks_data.to_csv(start.strftime('%Y-%m-%d')+'-'+end.strftime('%Y-%m-%d')+'-'+str(len(vetted_symbols))+'stocks_data.csv', index = False)
-
-
-def stocks_table_function(**kwargs):
-
-    print('3 Creating aggregated dataframe with stock stats for last available date + write to CSV file...')
-
-    #ti = kwargs['ti']
-
-    #stocks_prices = ti.xcom_pull(task_ids='fetch_prices_task') # <-- xcom_pull is used to pull the stocks_prices list generated above
-    stocks_prices = stocks_data # <-- xcom_pull is used to pull the stocks_prices list generated above
-
-    stocks_adj_close = []
-
-    for i in range(0, len(vetted_symbols)):
-
-        #adj_price= stocks_prices[i][['Date','Adj Close']]
-
-        temp =  stocks_prices['Symbol']==vetted_symbols[i]
-        a=stocks_prices[temp]
-
-        adj_price = a[["Date","Adj Close"]]
-        #print(adj_price)
-
-        adj_price.set_index('Date', inplace = True)
-
-        adj_price.columns = [vetted_symbols[i]]
-
-        stocks_adj_close.append(adj_price)
-
-
-    stocks_adj_close = reduce(lambda left,right: pd.merge(left, right, left_index = True, right_index = True ,how='outer'), stocks_adj_close)
-
-    stocks_adj_close.sort_index(ascending = False, inplace = True)
-
-    stocks_adj_close.index = pd.to_datetime(stocks_adj_close.index).date
-
-
-
-    stocks_adj_close_f = stocks_adj_close.iloc[0] # <- creates a copy of the full df including last row only
-
-    stocks_adj_close_f = stocks_adj_close_f.reset_index() # <- removing the index transforms the pd.Series into pd.DataFrame
-
-    stocks_adj_close_f.insert(loc = 1, column = 'Date', value = stocks_adj_close_f.columns[1])
-
-    stocks_adj_close_f.columns = ['Symbol', 'Date' , 'Adj. Price']
-
-    stocks_adj_close_f.set_index('Symbol', inplace = True)
-
-
-
-
-    #######################################
-
-
-
-    def get_key_stats(tgt_website):
-
-        df_list = pd.read_html(tgt_website)
-
-        result_df = df_list[0]
-
-        for df in df_list[1:]:
-
-             result_df = result_df.append(df)
-
-        return result_df.set_index(0).T
-
-
-
-    stats = pd.DataFrame()
-
-    statistics = []
-
-
-
-    for i in range(0, len(vetted_symbols)):
-
-        values =(get_key_stats('https://sg.finance.yahoo.com/quote/'+ str(vetted_symbols[i]) +'/key-statistics?p='+ str(vetted_symbols[i])))
-
-        statistics.append(values)
-
-
-
-    stats = stats.append(statistics)
-
-    stats.reset_index(drop=True, inplace= True)
-
-    stats.insert(loc=0, column='Symbol', value=pd.Series(stocks)) 
-
-    stats.set_index(['Symbol'], inplace = True)
-
-
-
-    stats = stats[['Market cap (intra-day) 5', 'Enterprise value 3', 'Trailing P/E',
-
-                    'Forward P/E 1', 'PEG Ratio (5 yr expected) 1', 'Price/sales (ttm)',
-
-                    'Price/book (mrq)', 'Enterprise value/revenue 3',
-
-                    'Enterprise value/EBITDA 6', 'Beta (5Y monthly)', '52-week change 3',
-
-                    'S&P500 52-week change 3', '52-week high 3', '52-week low 3',
-
-                    '50-day moving average 3', '200-day moving average 3',
-
-                    'Avg vol (3-month) 3', 'Avg vol (10-day) 3', 'Shares outstanding 5',
-
-                    'Float', '% held by insiders 1', '% held by institutions 1',
-
-                    'Forward annual dividend rate 4', 'Forward annual dividend yield 4',
-
-                    'Trailing annual dividend rate 3', 'Trailing annual dividend yield 3',
-
-                    '5-year average dividend yield 4', 'Payout ratio 4', 'Dividend date 3',
-
-                    'Ex-dividend date 4', 'Last split factor 2', 'Last split date 3',
-
-                    'Fiscal year ends', 'Most-recent quarter (mrq)', 'Profit margin',
-
-                    'Operating margin (ttm)', 'Return on assets (ttm)',
-
-                    'Return on equity (ttm)', 'Revenue (ttm)', 'Revenue per share (ttm)',
-
-                    'Quarterly revenue growth (yoy)', 'Gross profit (ttm)', 'EBITDA',
-
-                    'Net income avi to common (ttm)', 'Diluted EPS (ttm)',
-
-                    'Quarterly earnings growth (yoy)', 'Total cash (mrq)',
-
-                    'Total cash per share (mrq)', 'Total debt (mrq)',
-
-                    'Total debt/equity (mrq)', 'Current ratio (mrq)',
-
-                    'Book value per share (mrq)', 'Operating cash flow (ttm)',
-
-                    'Levered free cash flow (ttm)']]
-
-
-
-    stats.columns = ['Mkt_Cap', 'Enterpr_Value', 'P/E',
-
-                      'P/E_(Forward)', 'PEG_Ratio', 'P/S',
-
-                      'P/B', 'Enterpr_Value/Revenue','Enterpr_Value/EBITDA', 
-
-                      'Beta_(5Y M)', '52W Change','S&P500_52W_Change', '52W_High', '52W_Low',
-
-                      '50D Mov_Avg', '200D Mov_Avg.',
-
-                      'Avg_Vol_(3M)', 'Avg_Vol_(10D)', 'Outst_Shares',
-
-                      'Float', 'Insiders_Stake_pct', 'Institutions_Stake_pct',
-
-                      'Dividend Rate_(F Annual)', 'Dividend Yield_(F Annual)',
-
-                      'Dividend Rate_(T Annual)', 'Dividend_Yield_(T Annual)',
-
-                      'Dividend Yield_(Avg 5Y)', 'Payout_Ratio', 'Dividend_Date',
-
-                      'Ex-dividend_Date', 'Last_Split_Factor', 'Last_Split_Date',
-
-                      'Fiscal_Year_Ends', 'MRQ', 'ProfMargin',
-
-                      'Operating_Margin', 'ROA', 'ROE', 'Revenue12M', 'Revenue_p/s_(12M)',
-
-                      'Quarterly_Revenue_Growth_(YoY)', 'Gross_Profit(L12M)', 
-
-                      'EBITDA','Net Income 12M', 'Diluted EPS_(L12M)',
-
-                      'Quarterly_Earnings_Growth_(YoY)', 'Total_Cash_(MRQ)',
-
-                      'Total_Cash_Per_Share_(MRQ)', 'Total_Debt_(MRQ)',
-
-                      'Total_Debt/Equity_(MRQ)', 'Current Ratio',
-
-                      'Book Value p/s_(MRQ)', 'Ops CF 12M',
-
-                      'Levered_Free_Cash_Flow_(L12M)']
-
-
-
-    stats_c = stats.copy()
-
-    stats_c = stats_c[['Mkt_Cap','P/B' ,'P/E', 'PEG_Ratio', 'ROA', 'ROE', 
-
-                           'Revenue12M', 'Net Income 12M', 'ProfMargin', 'Ops CF 12M', 'Current Ratio']]
-
-
-
-    stocks_table_data = pd.merge(stocks_adj_close_f, stats_c, left_index=True, right_index=True)
-
-    
-
-    print('DF Shape: ', stocks_table_data.shape)
-
-
-    stocks_table_data.to_csv('~stocks_table_data.csv', index = False)
-
-    return(stocks_table_data) 
-    #print('Completed')
-
-
-tbl = stocks_table_function()
 
 returnsdf = pd.DataFrame()
 returnsl = []
@@ -505,7 +307,10 @@ strats = {
     #"rsi": {"rsi_lower": 30, "rsi_upper": 70},
     #"macd": {"fast_period": 12, "slow_period": 26, "signal_period": 9, "sma_period": 30, "dir_period": 10},
     #"bbands": {"period": 20, "devfactor": 2.0},
+    
+    #needs to be custom
     #"ema": {"fast_period": 10, "slow_period": 30},
+    
     "custom": {"upper_limit": 1.5, "lower_limit":-1.5}
 } 
 
@@ -514,6 +319,7 @@ strats_opt = {
     #"rsi": {"rsi_lower": [15, 30], "rsi_upper": [70, 85]},
     #"macd": {"fast_period": [12], "slow_period": [26], "signal_period": [9], "sma_period": [30], "dir_period": [10]},
     #"bbands": {"period": [20], "devfactor": [2.0]},
+    
     #"ema": {"fast_period": [9,10,12], "slow_period": [30, 40, 50]},     
     "custom": {"upper_limit": [1.5], "lower_limit":[-1.5]}
 }         
@@ -558,7 +364,6 @@ res_data = pd.DataFrame()
 for i in range(0,len(res)):
     res_data = pd.concat([res[i][0],res_data])
     
-
 res_data.to_csv(start.strftime('%Y-%m-%d')+'-'+end.strftime('%Y-%m-%d')+'-'+str(len(vetted_symbols))+'res_backtest_data.csv', index = False)
 res_data
 
@@ -594,10 +399,14 @@ for i in range(0,len(stocklist)):
     #print(res[i])
     res_data = res[i][1]
     #print(res[i][1]['orders'][res[i][1]['orders']['strat_id']==choice])
-    print(res_data['orders'][res_data['orders']['strat_id']==choice])
+    print(stocklist[i])
+    
+    orders = res_data['orders'][res_data['orders']['strat_id']==choice]
+    #print(df.to_markdown(orders))
+    
     #values = res_data['periodic']['portfolio_value']
     values = res_data['periodic'][res_data['periodic']['strat_id']==choice]['portfolio_value']
-    print(len(values))
+    #print(len(values))
     ret_value = values.pct_change()[1:]
     cumulative_ret_value = (ret_value + 1).cumprod()
     
@@ -619,5 +428,39 @@ for i in range(0,len(stocklist)):
     
     plt.plot(cumulative_ret_value)
     plt.plot(cumulative_og_ret_data)
-    plt.show()
     
+    plt.show()
+    display(orders[["dt","type","price","size","value","commission","pnl"]])
+
+tgt_website = r'https://sg.finance.yahoo.com/quote/WDC/key-statistics?p=WDC'
+ 
+def get_key_stats(tgt_website):
+ 
+    # The web page is make up of several html table. By calling read_html function.
+    # all the tables are retrieved in dataframe format.
+    # Next is to append all the table and transpose it to give a nice one row data.
+    df_list = pd.read_html(tgt_website)
+    result_df = df_list[0]
+ 
+    for df in df_list[1:]:
+        result_df = result_df.append(df)
+ 
+    # The data is in column format.
+    # Transpose the result to make all data in single row
+    return result_df.set_index(0).T
+
+all_result_df = pd.DataFrame()
+url_prefix = 'https://sg.finance.yahoo.com/quote/{0}/key-statistics?p={0}'
+for sym in stocklist:
+    stock_url = url_prefix.format(sym)
+    result_df = get_key_stats(stock_url)
+    if len(all_result_df) ==0:
+        all_result_df = result_df
+    else:
+        all_result_df = all_result_df.append(result_df)
+
+all_result_df.dropna(axis=0, how='any')
+all_result_df['Symbol'] = stocklist
+# Save all results
+all_result_df.to_csv('results.csv', index =False)
+all_result_df    
