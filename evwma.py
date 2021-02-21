d70ce8852a70f6445c512bf052814eb6b81af0f8
My digital passions <laferrierejc@gmail.com>
2021-02-21 12:27:35 -0600

    evwma strategy
    updated orderbook to calculate a final portfolio value

---
+++ evwma.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+get_ipython().system('pip install pandas_ta dataclasses')
+
+
+# In[ ]:
+
+
+pd.set_option('display.max_columns', None) #replace n with the number of columns you want to see completely
+pd.set_option('display.max_rows', None) #replace n with the number of rows you want to see completely
+
+from finta import TA
+import pandas_ta as ta
+from fastquant import get_crypto_data, get_stock_data, backtest
+from fbprophet import Prophet
+from matplotlib import pyplot as plt
+from fbprophet.diagnostics import cross_validation
+from sklearn.metrics import mean_squared_error
+import numpy as np
+import pandas as pd
+
+def wwma(values, n):
+    """
+     J. Welles Wilder's EMA 
+    """
+    return values.ewm(alpha=1/n, adjust=False).mean()
+
+def atr(df, n=50):
+    data = df.copy()
+    high = data['high']
+    low = data['low']
+    close = data['close']
+    data['tr0'] = abs(high - low)
+    data['tr1'] = abs(high - close.shift())
+    data['tr2'] = abs(low - close.shift())
+    tr = data[['tr0', 'tr1', 'tr2']].max(axis=1)
+    atr = wwma(tr, n)
+    return atr
+
+#max # of records is 500
+outter_df = get_crypto_data("BTC/USDT", "2019-01-01", "2020-05-31")
+
+#EVWMA
+Short_EVWMA = pd.DataFrame(TA.EVWMA(outter_df,12))
+Long_EVWMA = pd.DataFrame(TA.EVWMA(outter_df,26))
+Short_EVWMA.columns = ['EVWMA_12']
+Long_EVWMA.columns = ['EVWMA_26']
+
+ATR = pd.DataFrame(atr(outter_df))
+ATR.columns = ['ATR']
+
+#p 209 of ttr doc
+MACD_EVWMA = pd.DataFrame(Short_EVWMA['EVWMA_12'] - Long_EVWMA['EVWMA_26'])
+MACD_EVWMA.columns = ['MACD-line']
+
+Signal_EVWMA = pd.DataFrame(ta.ema(MACD_EVWMA["MACD-line"], length=9))
+Signal_EVWMA.columns = ['Signal_EMA_9_MACD']
+Signal_EVWMA
+outter_df['custom'] = Signal_EVWMA['Signal_EMA_9_MACD']
+
+
+df.to_csv('btresults.csv', index =True)
+
+
+# In[ ]:
+
+
+outter_df.to_csv('btresults.csv', index =True)
+
--- orderBook.py
+++ orderBook.py
@@ -1,33 +1,40 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+pd.set_option('display.max_columns', None) #replace n with the number of columns you want to see completely
+pd.set_option('display.max_rows', None) #replace n with the number of rows you want to see completely
+
 import pandas as pd
 from fastquant import backtest
+
+from matplotlib import pyplot as plt
 
 df = pd.read_csv('btresults.csv')
 df = df.set_index('dt')
 
 #inverse
-df['custom'] = df['custom']
+df['custom'] = df['custom']*1
 
 #actualReturns = ((df['close'].shift(-1)-df['close'])/df['close'])*100
-
 
 mergeCompare = pd.merge(df['close'].shift(+1),df['close'], how='inner', left_index=True, right_index=True)
 
 actualReturns = ((mergeCompare['close_y']-mergeCompare['close_x'])/mergeCompare['close_x'])
 
-
 #actualReturns = ((mergeCompare['close_x']-mergeCompare['close_y'])/mergeCompare['close_y'])
-
 
 orderbook = pd.DataFrame()
 
 funds = 1000
-BuyFundsPercent = .1
-percentFundsOnSell = .5
+BuyFundsPercent = .25
+percentHeldOnSell = 1
 
 held = 0
-upper = .5
-lower = -.5
-
+upper = 0
+lower = 0
 
 for i in df.index:
     #print(df.loc[i]['custom'])
@@ -48,7 +55,7 @@ for i in df.index:
     elif estRet < lower:        
         temp['order'] = ['sell']
 
-        Qty = held*percentFundsOnSell
+        Qty = held*percentHeldOnSell
         value = df.loc[i]['close']*Qty
         
         funds = funds + value
@@ -72,13 +79,15 @@ for i in df.index:
     temp['PropInvValue'] = df.loc[i]['close']*Qty
     temp['qtyShares'] = Qty
     temp['funds'] = funds
+    temp['portValue'] = funds + df.loc[i]['close']*held
     temp['held'] = held
+    
+    temp = temp.round(2)
 
     orderbook = orderbook.append(temp)
 
-display(orderbook)
-#print(held)
-#print(funds)
+display(orderbook.dropna().head())
+display(orderbook.dropna().tail())
 
 import matplotlib.pyplot as plt
 plt.scatter(orderbook['estRet'], orderbook['actRet'],label="x: estRet; y:actRet")
@@ -88,12 +97,29 @@ plt.ylabel('actRet')
 from scipy.stats import pearsonr
 
 #cov(orderbook['estRet'], orderbook['actRet'])
-corr, _ = pearsonr(orderbook['estRet'][1:], orderbook['actRet'][1:])
-print('Pearsons correlation: %.3f' % corr)
-#df
+corrprep = pd.merge(orderbook.set_index('date')['estRet'][1:],orderbook.set_index('date')['actRet'][1:], how='inner', left_index=True, right_index=True)
+corr, _ = pearsonr(corrprep.dropna()['estRet'][1:], corrprep.dropna()['actRet'][1:])
+print('Pearsons correlation: %.3f' % corr)#df
 
 print("Total Return")
-print(orderbook['funds'][-1:].values[0]/1000)
+print(orderbook['portValue'][-1:].values[0]/1000)
 
 print("Hold Return")
 print(df['close'][-1:].values[0]/df['close'][0:].values[0])
+
+#plt.plot(orderbook.set_index('date')['portValue'])
+
+values = orderbook.set_index('date')['portValue']
+#print(len(values))
+ret_value = values.pct_change()[1:]
+cumulative_ret_value = (ret_value + 1).cumprod()
+
+#show cumulative charts
+og_ret_data = orderbook.set_index('date')['price']
+og_ret_value = og_ret_data.pct_change()[1:]
+#og_merge = pd.merge(df,custom.set_index('dt'), how='inner', left_index=True, right_index=True)
+cumulative_og_ret_data = (og_ret_value + 1).cumprod()
+
+plt.plot(cumulative_ret_value)
+plt.plot(cumulative_og_ret_data)
+
