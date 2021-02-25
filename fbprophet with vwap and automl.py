5f30c597722b1dd41371dffd9f26037e81f3d353
My digital passions <laferrierejc@gmail.com>
2021-02-25 07:41:23 -0600

    Update fbprophet with vwap and automl.py

--- fbprophet with vwap and automl.py
+++ fbprophet with vwap and automl.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[25]:
 
 
-get_ipython().system('pip install keras numpy sklearn auto_ml tensorflow')
+get_ipython().system('pip install keras numpy sklearn auto_ml tensorflow fredapi')
 
 
-# In[2]:
+# In[26]:
 
 
 #plt.plot(pred['yhat_lower'])
@@ -15,7 +15,7 @@ get_ipython().system('pip install keras numpy sklearn auto_ml tensorflow')
 #plt.plot(pred['yhat_upper'])
 
 
-# In[3]:
+# In[27]:
 
 
 #from fastquant import get_stock_data
@@ -25,7 +25,40 @@ get_ipython().system('pip install keras numpy sklearn auto_ml tensorflow')
 #outter_df
 
 
-# In[4]:
+# In[28]:
+
+
+etf_commodities = ['DBO','CORN', 'WEAT', 'SOYB', 'JO', 'SGG', 'BAL', 'COW', 'MOO', 'TAGS', 'KOL' ]
+#Gold, Silver, Platinum, Copper, Paladium, Aluminum, Iron, Steel
+etf_metals = ['IAU', 'SLV', 'PGM', 'JJC', 'PALL', 'JJU', 'IFUNX', 'SLX']
+#US dollar, European Euro, Japanese yen, Pound sterling, Australian dollar, Canadian dollar, Swiss franc, Chinese Yuan Renminbi, Swedish Krona, Peso, India
+#defunct: Russia: XRU, Mexico: FXM
+etf_foreign_exchanges = ['UUP','FXE','FXY','FXB','FXA','FXC','FXF','CYB', 'FXS', 'INR']
+#residential, Ishares all NAmerica
+etf_real_estate = ['REZ', 'IYR']
+#Russia, Germany, UK, Japan, China, Euro, Euro, Brazil, Latin America, Mexico, India
+etf_economies = ['ERUS','EWG','EWU','EWJ','MCHI','EZU','IEUR','EWZ','ILF','EWW','INDA']
+#Ishares Investment Grade, IShares core aggregate Investment grade, Short, Total, 1-5 Years, 5-10 Years, 10 Years, Gov/Credit
+#defunct:  
+
+etf_spdr_indexes = ['XLC','XLY','XLP','XLE','XLF','XLV','XLI','XLB','XLRE','XLK','XLU']
+etf_dow_components = ['MMM','AXP','AMGN','AAPL','BA','CAT','CVX','CSCO','KO','DOW','GS','HD','HON','IBM','INTC','JNJ','JPM','MCD','MRK','MSFT','NKE','PG','CRM','TRV','UNH','VZ','V','WMT','WBA','DIS']
+
+etf_bonds = ['LQD', 'AGG', 'NEAR', 'IUSB', 'ISTB', 'IMTB', 'ILTB', 'GBF']
+etf_muni_bonds = ['MUB', 'SUB', 'MEAR']
+
+etf_treasuries = ['AGZ', 'GOVT', 'BIL', 'SHV', 'SHY', 'IEI', 'IEF', 'TLT']
+
+crypto = ['BTCUSD=X','ETH','RPL','BCH','EOS','LTC']
+
+etf_and_Crypto_list = [[etf_commodities, etf_metals, etf_foreign_exchanges, etf_real_estate, etf_economies, etf_bonds, etf_muni_bonds, etf_treasuries, crypto, etf_spdr_indexes, etf_dow_components]]
+#,'GOLDAMGBD228NLBM',
+FRED_Indicators = ['CPALTT01USQ657N','PAYEMS','IRLTLT01USM156N','MABMM301USM189S','LFWA64TTUSM647S','MANMM101USA189S','MICH','UMCSENT','CSCICP03USM665S','DGS10','DTB3','DGS3MO','CASTHPI','GDPC1','CIVPART','POPTOTUSA647NWDB','MEHOINUSA672N','HOSMEDUSM052N','MORTGAGE30US','TTLHH','CSUSHPINSA','EMRATIO','CPIAUCSL','PSAVERT','LRUN64TTUSQ156S','USSTHPI','NYSTHPI','M2V','GFDEBTN','DFII10','GFDEGDQ188S','CUSR0000SEHA','ETOTALUSQ176N','ERENTUSQ176N','RECPROUSM156N','T5YIFR','BAMLHYH0A0HYM2TRIV','BAMLCC0A1AAATRIV','GVZCLS','DGS1','BAMLCC0A4BBBTRIV','VXVCLS','IC4WSA','WILLMICROCAPPR','WILLLRGCAPVAL','CFNAIDIFF','MZMSL','KCFSI','T5YIE','TOTALSA','DCOILWTICO','USSLIND','AWHAETP','CES0500000003','TCU','WTB3MS','WGS3MO','TWEXB','DEXCHUS','DEXUSUK','CILACBQ158SBOG','CES4348400001','FEDFUNDS','TDSP','PERMIT','GFDEGDQ188S','CP','PRFI','DRSFRMACBS','DRCCLACBS','DRBLACBS','DALLCIACBEP','USROA','USROE','RSAHORUSQ156S','MEFAINUSA672N','COMREPUSQ159N','HDTGPDUSQ163N','POP','NROU','FGCCSAQ027S','TEDRATE', 'VIXCLS', 'NFCI','INDPRO','LES1252881600Q','CUUR0000SEHA','LEU0252918500Q','BAA10Y','BAMLC0A0CM','BAMLH0A3HYC','BOGMBASE','DCOILBRENTEU','DCOILWTICO','DFF','DGS1MO','DGS30','DGS5','FPCPITOTLZGUSA','ICSA','INTDSRUSM193N','M1','M1V','MPRIME','PPIACO','SPCS20RSA','STLFSI2','T10Y2Y','T10Y3M','TB3MS','TREAST','UNRATE','WPU0911']
+
+Indexes = ['^SP500TR', 'QQQ', 'DIA', 'VTWO']
+
+
+# In[29]:
 
 
 from finta import TA
@@ -40,6 +73,7 @@ import datetime
 from datetime import timedelta
 from datetime import date
 import yfinance as yf
+from fredapi import Fred
 
 from sklearn.model_selection import train_test_split
 from scipy.stats import pearsonr
@@ -53,6 +87,7 @@ from auto_ml.utils_models import load_ml_model
 pd.set_option('display.max_columns', None) #replace n with the number of columns you want to see completely
 pd.set_option('display.max_rows', None) #replace n with the number of rows you want to see completely
 
+fred = Fred(api_key='661c0a90e914477da5a7518293de5f8e')
 
 end = datetime.date.today()
 start = end - timedelta(weeks=117)
@@ -158,14 +193,84 @@ df['Signal_EVWMA_x_VWP'] = df['Signal_EVWMA']*df['VWP']
 df['next_day_close'] = df['close'].shift(-1)
 
 
-# In[ ]:
+# In[30]:
+
+
+fred_data = pd.DataFrame()
+
+for x in range(0,len(FRED_Indicators)):
+    name = FRED_Indicators[x]
+    values = pd.DataFrame(fred.get_series(name))
+    values['Symbol'] = name
+    #values['Date'] = values.index
+    values = values.loc[~values.index.duplicated(keep='last')]        
+    values = values.reset_index()
+    values.columns = ['date','value','symbol']
+    #prices = prices.loc[~prices.index.duplicated(keep='last')]        
+    #
+                
+    fred_data = pd.concat([fred_data,values])    
+
+
+# In[31]:
+
+
+idx2 = df.index
+
+fred_data_cleaned = pd.DataFrame()
+fred_data_cleaned['date'] = df.index
+fred_data_cleaned = fred_data_cleaned.set_index('date')
+
+#creates pivot
+for x in FRED_Indicators:
+    #print(x)
+                
+    subset = fred_data[fred_data["symbol"]==x]
+    subset = subset[(subset['date']>= start) & (subset['date']<= end)]
+    
+    subset = subset.set_index('date')
+    subset = subset.loc[~subset.index.duplicated(keep='last')]        
+    #subset = subset.reset_index()
+    
+    idx1 = subset.index
+    
+    #merged = pd.merge(df,subset, how='inner', left_index=True, right_index=True)
+    
+    merged = idx1.union(idx2)
+    
+    set = subset.reindex(merged)
+    
+    df2 = set['value'].interpolate().dropna(axis=0, how='any')
+    
+    #if len(df2) == len(subset.index):
+        #print("true")
+        #fred_data_cleaned = pd.concat([fred_data_cleaned,df2])
+    fred_data_cleaned[x] = pd.DataFrame(df2)
+          
+
+
+# In[32]:
+
+
+df = pd.merge(fred_data_cleaned,df, how='inner', left_index=True, right_index=True)
+
+
+# In[41]:
 
 
 
 
 
-# In[5]:
+# In[39]:
 
+
+
+
+
+# In[42]:
+
+
+setlist = list(df[df.columns.drop('next_day_close')].columns)
 
 custompre = pd.DataFrame()
 
@@ -175,28 +280,31 @@ for x in range(0,len(df)):
     if length == oneyroneqtr:
         #print(outter_df.loc[frames])
         
-        df_inner = df.loc[frames]
+        #this needs to be -2
+        df_inner = df.loc[frames][0:-2]
         
-        ts = df_inner.reset_index()[["dt", "VWP"]]
+        #ts = df_inner.reset_index()[["dt", "VWP"]]
+        ts = df_inner[["date", "VWP"]]
         ts.columns = ['ds', 'y']
 
-        #strips last day from model
+        #no need to strip last day from model becuase df_inner (which ts is based on) is already -2
         m = Prophet(daily_seasonality=True,yearly_seasonality=True)
         m.add_seasonality(name='monthly', period=30.5, fourier_order=5)
         m.add_seasonality(name='quarterly', period=91.25, fourier_order=7)
-        m.fit(ts[0:-1])
+        #m.fit(ts[0:-1])
+        m.fit(ts)
 
-        #forecast only last day in model (can verify result)
-        forecast = pd.DataFrame(df_inner.index)[-1:]
+        #this needs to be -1
+        #forecast only last day in model (can verify result) needs to be based on df.loc (similar to df_inner)
+        forecast = pd.DataFrame(df.loc[frames].index)[-1:]
         forecast.columns = ['ds']
         
         #Predict and plot
         pred = m.predict(forecast)
                 
-        X = df_inner[['date', 'open', 'high', 'low', 'close', 'volume', 'Signal_EVWMA',
-       'RSI', 'bbands_upper', 'bbands_lower', 'ATR', 'VWP',
-       'bbands_upper_dv_VWP', 'bbands_lower_dv_VWP', 'bbands_mid',
-       'bbands_mid_dv_VWP', 'Signal_EVWMA_x_VWP']]
+        
+        X = pd.DataFrame(df_inner, columns=setlist)
+        #X = df_inner[['date', 'open', 'high', 'low', 'close', 'volume', 'Signal_EVWMA','RSI', 'bbands_upper', 'bbands_lower', 'ATR', 'VWP','bbands_upper_dv_VWP', 'bbands_lower_dv_VWP', 'bbands_mid','bbands_mid_dv_VWP', 'Signal_EVWMA_x_VWP']]
 
         y = df_inner['next_day_close']
 
@@ -247,7 +355,7 @@ for x in range(0,len(df)):
         
         temp = pd.DataFrame()
         #need date -2 back because we're predicting next day's return. Else I have to do shifting later
-        temp['date'] = [frames[-2].strftime('%Y-%m-%d')]
+        temp['date'] = [frames[-1].strftime('%Y-%m-%d')]
         temp['yhat'] = predictions2['custom'][-1:].values[0]
         
         custompre = custompre.append(temp, ignore_index=True)
@@ -256,7 +364,7 @@ for x in range(0,len(df)):
         break
 
 
-# In[6]:
+# In[43]:
 
 
 
@@ -264,7 +372,13 @@ merge=pd.merge(custompre.set_index('date').dropna(),df, how='inner', left_index=
 plt.scatter(merge['yhat'],merge['next_day_close'])
 
 
-# In[7]:
+# In[47]:
+
+
+merge
+
+
+# In[44]:
 
 
 
@@ -274,13 +388,29 @@ expected_1day_return = ((merge['yhat']-merge['close'])/merge['close']).multiply(
 merge["custom"] = expected_1day_return
 
 
-# In[9]:
+# In[ ]:
 
 
-merge
 
 
-# In[25]:
+
+# In[ ]:
+
+
+#propensities = pd.DataFrame()
+#propensities = corrprep
+#propensities['actRet'] = propensities['actRet']>0
+#propensities['actRet'].replace(>0,1)
+#propensities['actRet'].replace(<0,-1)
+#propensities[propensities['actRet'] > 0] = 1
+#propensities[propensities['actRet'] < 0] = -1
+#corrprep
+#propensities['actRet']
+
+#propensities.sort_values(by=['estRet'], ascending=False)
+
+
+# In[56]:
 
 
 #df = pd.read_csv('btresults.csv')
@@ -296,8 +426,9 @@ actualReturns = ((merge['next_day_close']-merge['close'])/merge['close'])
 orderbook = pd.DataFrame()
 
 funds = 1000
-BuyFundsPercent = .25
-percentSoldOfHeld = .75
+#this is how you leverage (you borrow money at a lower rate than you expect on your return)
+BuyFundsPercent = 1
+percentSoldOfHeld = 1
 
 held = 0
 upper = 0
@@ -324,7 +455,7 @@ for i in merge.index:
         held = held + Qty
                 
     #what about over 80 RSI?  Guess it's a hold until it drops?
-    elif (estRet < lower):
+    elif (estRet <= lower):
     #elif (estRet < lower and rsi < 80 and rsiDelta < 0)
     #elif (estRet < lower and rsi < 80 and rsiDelta < 0 and (df.loc[i]['close']*df.loc[i]['volume'] > df.loc[i]['bbands_lower'])):
         temp['order'] = ['sell']
@@ -402,8 +533,28 @@ plt.plot(merge.dropna()['bbands_lower'])
 plt.show()
 
 
+# In[59]:
+
+
+
+#residuals = merge['yhat']-merge['next_day_close']
+residuals = orderbook['estRet']-orderbook['actRet']
+
+
+_ = plt.hist(residuals, bins='auto')  # arguments are passed to np.histogram
+plt.title("Histogram with 'auto' bins")
+plt.show()
+
+
+# In[57]:
+
+
+plt.plot(cumulative_ret_value)
+
+
 # In[ ]:
 
 
-
+print(custompre[-1:])
+print("Expected return",merge['custom'][-1:].values[0])
 
