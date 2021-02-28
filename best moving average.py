1fcf63fe1d20584959bd8008d3c8eb9b2c061b9e
My digital passions <laferrierejc@gmail.com>
2021-02-27 20:30:42 -0600

    Update best moving average.py
    finally finished setting up proper backtest

--- best moving average.py
+++ best moving average.py
@@ -1,25 +1,19 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import yfinance
 import pandas as pd
-import numpy as pd
+import numpy as np
 import matplotlib.pyplot as plt
+import datetime as dt
 import datetime
 from datetime import timedelta
 from datetime import date
 
 from scipy.stats import ttest_ind
 
-
-# In[116]:
-
-
 n_forward = 7
-name = 'BTC-USD'
+#name = 'BTC-USD'
+#name = 'GLD'
+name = 'SPY'
+name = 'GOOG'
 
 w=117
 end_date = datetime.date.today()
@@ -33,28 +27,21 @@ data = ticker.history(interval="1d",start=start_date,end=end_date, auto_adjust=T
 data['Forward Close'] = data['Close'].shift(-n_forward)
 data['Forward Return'] = (data['Forward Close'] - data['Close'])/data['Close']
 
-
-# In[117]:
-
-
-
-
-
-# In[73]:
-
-
 dateindex = data.loc[start_date:end_date].index
 
 dateindex
-
-
-# In[181]:
-
 
 limit = 100
 n_forward = 7
 
 train_size = 0.5
+
+trades = []
+expectedReturns = []
+
+sdevs = []
+
+minExpectedReturn = 0
 
 width1 = len(data.loc[start_date:end_date1].index)
 width2 = len(data.loc[end_date1+timedelta(days=1):end_date].index)
@@ -96,10 +83,15 @@ for i in range(0,width1):
     
     if result[0]['p-value'] > .1:
         #print(result[0]['p-value'])
-        if result[0]['training_forward_return'] > 0:
-            if result[0]['test_forward_return'] > 0:
+        if result[0]['training_forward_return'] > minExpectedReturn:
+            if result[0]['test_forward_return'] > minExpectedReturn:
                 if temp.ix[-1]['Close']>temp.ix[-1]['SMA']:
-        
+                    
+                    #add to list of trades
+                    trades.append(temp.index[-1].strftime('%Y-%m-%d'))
+                    expectedReturns.append((result[0]['training_forward_return']+result[0]['test_forward_return'])/2)
+                    sdevs.append(np.std(temp['Forward Return']))
+
                     print(result[0])
                     print(temp[-1:]['Close'])
                     print(temp[-1:]['SMA'])
@@ -107,53 +99,233 @@ for i in range(0,width1):
                     plt.plot(temp['Close'],label='BTC')
 
                     stringLabel = str(result[0]['sma_length']) + " SMA" + " at " + str(n_forward) + " day return " + str(result[0]['test_forward_return'].round(3))
-                    plt.plot(data['Close'].rolling(result[0]['sma_length']).mean(),label = stringLabel)
+                    #plt.plot(data['Close'].rolling(result[0]['sma_length']).mean(),label = stringLabel)
+                    plt.plot(temp['Close'].rolling(result[0]['sma_length']).mean(),label = stringLabel)
+                    
                     plt.legend()
 
                     plt.show()
+                    
+                    plt.hist(temp['Forward Return'], bins='auto')  # arguments are passed to np.histogram
+                    plt.show()
         
         
+plt.hist(sdevs, bins='auto')  # arguments are passed to np.histogram
+plt.show()
+plt.hist(expectedReturns, bins='auto')  # arguments are passed to np.histogram
+plt.show()
     
 
+start = 1000
 
-# In[176]:
+set = pd.DataFrame()
+for i in range(0,len(trades)):
+    
+    value = pd.DataFrame(data.loc[trades[i]]).transpose()
+    value['ExpectedReturn'] = expectedReturns[i]
+    value['sdev'] = sdevs[i]
+    set = pd.concat([set,value])
+    #funds = 
 
-
-
-
-
-# In[179]:
-
-
-
-
-
-# In[97]:
+plt.hist(set['Forward Return'], bins='auto')  # arguments are passed to np.histogram
 
 
 
+orderbook = pd.DataFrame()
+
+dateindex2 = data.loc[end_date1:end_date].index
+
+#temp = pd.DataFrame([dateToBeSold,1],columns=['date','qty'])
+column_names = ["date", "qty"]
+
+sellDates = pd.DataFrame(columns = column_names)
+
+#set[dateindex2[1].strftime('%Y-%m-%d')]
+for i in dateindex2:
+    
+    idate = i.strftime('%Y-%m-%d')        
+    
+    #process purchases
+    if (idate in set.index):
+
+        temp = pd.DataFrame()
+        
+        estRet = set.loc[idate]['ExpectedReturn']
+
+        temp['orderside'] = ['buy']        
+
+        ProportionOfFunds = funds * BuyFundsPercent
+        #Qty = ProportionOfFunds / set.loc[idate]['Close']
+        paid = set.loc[idate]['Close']*Qty
+        
+        if len(data[start_date:idate])-1+n_forward>=len(data[start_date:]):
+            dateToBesold = np.nan    
+            temp['valueAtSale'] = np.nan
+        else:
+            dateToBeSold = data.ix[len(data[start_date:idate])-1+n_forward].name.strftime('%Y-%m-%d') 
+            
+            temp['valueAtSale'] = pd.DataFrame(data.ix[len(data[start_date:idate])-1+n_forward]).transpose()['Close'].values[0]            
+         
+        temp['date'] = [idate]
+        temp['valueAtPurchase'] = set.loc[idate]['Close']
+        temp['estRet'] = estRet
+        #temp['qty'] = Qty
+        temp['dateBought'] = idate        
+        temp['dateToBeSold'] = dateToBeSold
+        
+
+        btemp = pd.DataFrame(columns = column_names)
+        btemp["date"]=[dateToBeSold]
+        #btemp["qty"]=[Qty]
+
+        sellDates = sellDates.append(btemp,ignore_index=True)
+  
+        temp = temp.round(4)
+
+        orderbook = orderbook.append(temp,ignore_index=True)
+        
+for i in dateindex2:
+    
+    idate = i.strftime('%Y-%m-%d')        
+    
+    #process sales
+        
+    if (idate in sellDates.set_index('date').index):    
+        temp = pd.DataFrame()
+
+        dateBought = data.ix[len(data[start_date:idate])-1-n_forward].name.strftime('%Y-%m-%d')        
+        dateToBeSold = idate
+        temp['dateBought'] = [dateBought]
+        temp['dateToBeSold'] = dateToBeSold
+        temp['valueAtPurchase'] = pd.DataFrame(data.ix[len(data[start_date:idate])-1-n_forward]).transpose()['Close'].values[0]
+        estRet = set.loc[dateBought]['ExpectedReturn']
+        temp['estRet'] = estRet
+        temp['valueAtSale'] = pd.DataFrame(data.ix[len(data[start_date:idate])-1]).transpose()['Close'].values[0]
+        
+        #temp['dateToBeSold'] = idate
+        #temp['estRet'] = data.loc[idate]['Forward Return']
+
+        temp['orderside'] = ['sell']        
+        temp['date'] = [idate]
+
+        #data vs set because set only includes buy dates
+        #temp['valueAtSale'] = pd.DataFrame(data.ix[len(data[start_date:idate])-1+n_forward]).transpose()['Close']
+
+        #temp['qty'] = sellDates.set_index('date').loc[idate]['qty']
+
+        temp = temp.round(4)
+
+        orderbook = orderbook.append(temp,ignore_index=True)
 
 
-# In[11]:
+orderbook.sort_values(by=['date','orderside'], ascending=True)
 
 
+funds = 1000
+BuyFundsPercent = .25
+percentHeldOnSell = 1
+
+buyLog = pd.DataFrame()
+sellLog = pd.DataFrame()
+runningLog = pd.DataFrame()
+
+held = 0
+upper = 0
+lower = 0
+
+for i in dateindex2:
+    
+    temp = pd.DataFrame()
+    rtemp = pd.DataFrame()
+    _temp = pd.DataFrame()
+    
+    t = i.strftime('%Y-%m-%d')
+    
+    subset = orderbook[orderbook['date']==t]
+    gain = 0
+    paid = 0
+    
+    if len(subset) != 0:
+        
+        sales = subset[subset['orderside'] == 'sell']
+        
+        print("date " + str(i))
+        
+        if len(sales) != 0:                        
+            
+            oldvalue = sales['valueAtPurchase'].values[0]
+
+            newvalue = sales['valueAtSale'].values[0]            
+            
+            Qty = buyLog.set_index('date').loc[sales['dateBought'].values[0]].values[0]
+            print("Qty sold " + str(Qty.round(2)))
+            
+            gain = newvalue * Qty
+            
+            _temp['date'] = [i]
+            _temp['qty'] = [Qty]
+            
+            sellLog = sellLog.append(_temp)
+    
+        purchases = subset[subset['orderside'] == 'buy']
+        
+        if len(purchases) != 0:
+    
+            ProportionOfFunds = funds * BuyFundsPercent
+        
+            Qty = ProportionOfFunds / purchases['valueAtPurchase'].values[0]
+            print(purchases['valueAtPurchase'].values[0])
+            print("Qty purchased " + str(Qty.round(2)))
+            
+            temp['date'] = [i]
+            temp['qty'] = [Qty]
+            
+            paid = purchases['valueAtPurchase'].values[0]*Qty
+                        
+            buyLog = buyLog.append(temp)
+        
+        funds = funds + gain - paid
+                    
+        rtemp['date'] =  [i]
+        rtemp['funds'] =  [funds]
+        
+        if len(sellLog) != 0:
+            remainder = (sum(buyLog['qty'])-sum(sellLog['qty']))            
+            
+        else:
+            remainder = (sum(buyLog['qty']))
+        
+        rtemp['held'] = remainder
+        rtemp['value'] = remainder * data.loc[i]['Close']
+        rtemp['portValue'] = funds + remainder * data.loc[i]['Close']
+                
+        print("in " + str(gain))
+        print("out " + str(paid))
+        print("held: " + str(remainder))
+        print("Close Value: " + str(data.loc[i]['Close']))
+        print("held Value: " + str(remainder * data.loc[i]['Close']))
+        print("funds " + str(funds))
+        print("portValue " + str(funds + remainder * data.loc[i]['Close']))
+        print()
+            
+        runningLog = runningLog.append(rtemp)
+        
 
 
+            
+        
+buyLog
+plt.plot(runningLog.set_index('date')['portValue'])
+#plt.plot(runningLog.set_index('date')['funds'])
+plt.xticks(rotation=30) 
+#subset
+#plt.plot(runningLog.set_index('date'))
 
-# In[12]:
+#display(orderbook.dropna())
+#plt.plot(orderbook.set_index('date').dropna()['portValue'])
+#plt.xticks(rotation=30) 
+#plt.show()
+#len(orderbook.dropna())
 
-
-#data['Close'].rolling(result[0]['sma_length']).mean()
-
-
-# In[13]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
+#plt.hist(orderbook['profit'], bins='auto')  # arguments are passed to np.histogram
+#orderbook['profit'].dropna().sum()
